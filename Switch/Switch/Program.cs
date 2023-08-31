05c4f294589d5393c71a111cbec5e6818a88b8d1
Nerd Dev <83460815+nerddev77@users.noreply.github.com>
2023-08-31 15:16:54 -0300

    Switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cor = "Azul";
+
+            switch (cor)
+            {
+                case "Vermelho":
+                    Console.WriteLine("Sua cor favorita é vermelho!");
+                    break;
+                case "Amarelo":
+                    Console.WriteLine("Sua cor favorita é amarelo!");
+                    break;
+                case "Azul":
+                    Console.WriteLine("Sua cor favorita é azul!");
+                    break;
+                default:
+                    Console.WriteLine("Sua cor favorita eu não sei...");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
