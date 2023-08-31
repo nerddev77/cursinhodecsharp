06a5b8d406375f985ebab043be0b15407a8d068b
Nerd Dev <83460815+nerddev77@users.noreply.github.com>
2023-08-31 15:16:15 -0300

    Hello World

---
+++ Hello World/AulaDe5Horas/AulaDe5Horas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaDe5Horas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroQualquer = 20 + 20 - 80;
+            int numeroMult = 10 * 90;
+            float numeroDiv = 5.0f / 2;
+            int teste = (2 + 2) * 10;
+
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMult);
+            Console.WriteLine(numeroDiv);
+            Console.WriteLine(teste);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Hello World/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            /*
+            int segundaGuerraMundial = 1942;
+            string corFavorita = "Verde";
+            float velocidadeF1 = 250.34f;
+            bool aconteceuSegundaGuerra = true;
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(aconteceuSegundaGuerra);
+            */
+            Console.WriteLine("Boa noite, qual seu nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+        }
+    }
+}
