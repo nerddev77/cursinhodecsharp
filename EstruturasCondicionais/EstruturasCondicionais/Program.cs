69a9514a5dcec8eb55c484ed2415c0e7736397b6
Nerd Dev <83460815+nerddev77@users.noreply.github.com>
2023-08-31 15:14:46 -0300

    EstruturasCondicionais

---
+++ EstruturasCondicionais/EstruturasCondicionais/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 2;
+
+            // &&(E/AND)
+            // || (OU/OR)
+            // Se a pessoa for MULHER && IDADE >= 25 -> Entra na festa de graça
+
+
+
+            if (a < b && a > c) 
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else if (a > c)
+                {
+                Console.WriteLine("Executou Else If");
+            }
+            else
+            {
+                Console.WriteLine("É mentira");
+            }
+            Console.ReadLine();
+        }
+    }
+}
