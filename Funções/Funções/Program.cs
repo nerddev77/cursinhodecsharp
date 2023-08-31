21342a98178d04ae25b2888c5400c7af129ff04f
Nerd Dev <83460815+nerddev77@users.noreply.github.com>
2023-08-31 15:15:33 -0300

    Funções

---
+++ Funções/Funções/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            /*
+            GerarPreco(60, "Curso de C#");
+            ExibirMsg();
+            ExibirMsg();
+            ExibirMsg();
+            
+            int soma1 = Somar(1,20,4);
+            */
+            string[] produtos = new string[5]
+            {
+                "Sea of Tghieves", //0
+                "Minecraft", //1
+                "FIFA", //2
+                "Half-Life", //3
+                "Portal" //4
+            };
+
+            Console.WriteLine(produtos[1]);
+
+            Console.ReadLine(); 
+        }
+        static void ExibirMsg()
+        {
+            Console.WriteLine("CSharp é show de bola");
+            Console.WriteLine("Estou usando uma função!");
+        }
+
+        static void GerarPreco(int preco, string nome)
+        {
+            Console.WriteLine("Nome do produto: " + nome);
+            Console.WriteLine(preco);
+        }
+
+        static int Somar(int a, int b, int c)
+        {
+            int resultadoFinal = a + b + c;
+            return resultadoFinal;
+        }
+    }
+}
